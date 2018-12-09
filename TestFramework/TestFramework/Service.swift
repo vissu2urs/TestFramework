cd5bcd8bdb0c67cd2d3167c5c14d04aa8bb33771
vishnu  vardhan <siddanatham.vishnuvardhan@gmail.com>
2018-12-09 23:41:19 +0530

    made first changes

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by vishnu  vardhan on 09/12/18.
+//  Copyright © 2018 vishnu  vardhan. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init { }
+    public static func doSomething() -> String {
+        return "vishnu vardhan did something"
+    }
+    
+    
+    
+}
---
+++ TestFramework/TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by vishnu  vardhan on 09/12/18.
+//  Copyright © 2018 vishnu  vardhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
